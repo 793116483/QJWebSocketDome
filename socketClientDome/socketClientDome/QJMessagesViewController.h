879422df9d8b9ed920a40adcaedcbced1c2b5fac
qujie <793116483@qq.com>
2017-09-07 14:08:20 +0800

    创建继承 JSQMessagesViewController 的 QJMessagesViewController 类

---
+++ socketClientDome/socketClientDome/QJMessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  QJMessagesViewController.h
+//  socketClientDome
+//
+//  Created by 瞿杰 on 2017/9/7.
+//  Copyright © 2017年 yiniu. All rights reserved.
+//
+
+#import <JSQMessagesViewController/JSQMessagesViewController.h>
+
+@interface QJMessagesViewController : JSQMessagesViewController
+
+@end
---
+++ socketClientDome/socketClientDome/QJMessagesViewController.m
@@ -0,0 +1,37 @@
+//
+//  QJMessagesViewController.m
+//  socketClientDome
+//
+//  Created by 瞿杰 on 2017/9/7.
+//  Copyright © 2017年 yiniu. All rights reserved.
+//
+
+#import "QJMessagesViewController.h"
+
+@interface QJMessagesViewController ()
+
+@end
+
+@implementation QJMessagesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
