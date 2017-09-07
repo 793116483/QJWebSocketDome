a89bfd60f825cc70043aa5c3ec6238ef3132dd7b
qujie <793116483@qq.com>
2017-09-07 18:24:21 +0800

    新建 处理 上传 和 接收服务器的数据

---
+++ socketClientDome/socketClientDome/QJHandleMessageModel.h
@@ -0,0 +1,24 @@
+//
+//  QJHandleMessageModel.h
+//  socketClientDome
+//
+//  Created by 瞿杰 on 2017/9/7.
+//  Copyright © 2017年 yiniu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QJHandleMessageModel : NSObject
+
+@property (nonatomic , copy) NSString * displayName ;
+
+@property (nonatomic , copy) NSString * senderId ;
+
+@property (nonatomic , copy) NSString * text ;
+
+@property (nonatomic , copy) NSString * dateStr ;
+
++(instancetype)handleMessageModelWithSenderId:(NSString *)senderId displayName:(NSString *)displayName text:(NSString *)text dateStr:(NSString*)dateStr ;
+
+
+@end
---
+++ socketClientDome/socketClientDome/QJHandleMessageModel.m
@@ -0,0 +1,25 @@
+//
+//  QJHandleMessageModel.m
+//  socketClientDome
+//
+//  Created by 瞿杰 on 2017/9/7.
+//  Copyright © 2017年 yiniu. All rights reserved.
+//
+
+#import "QJHandleMessageModel.h"
+
+@implementation QJHandleMessageModel
+
++(instancetype)handleMessageModelWithSenderId:(NSString *)senderId displayName:(NSString *)displayName text:(NSString *)text dateStr:(NSString *)dateStr
+{
+    QJHandleMessageModel * messageModel = [[self alloc] init];
+    
+    messageModel.senderId = senderId ;
+    messageModel.displayName = displayName ;
+    messageModel.text = text ;
+    messageModel.dateStr = dateStr ;
+    
+    return messageModel ;
+}
+
+@end
